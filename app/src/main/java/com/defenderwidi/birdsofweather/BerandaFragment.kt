03a9bc790e0299b4020344a330420a36e6c9d078
Defender_Widi <defenderwidi17@gmail.com>
2024-10-08 02:36:12 +0700

    implement page pemberitahuan, navigation

--- app/src/main/java/com/defenderwidi/birdsofweather/BerandaFragment.kt
+++ app/src/main/java/com/defenderwidi/birdsofweather/BerandaFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 
 class BerandaFragment : Fragment() {
+
+    private lateinit var notificon: ImageView
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -20,6 +24,13 @@ class BerandaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_beranda, container, false)
+
+        notificon=view.findViewById(R.id.iconnotif)
+
+        notificon.setOnClickListener{
+            findNavController().navigate(R.id.action_berandaFragment_to_pemberitahuanFragment)
+        }
         return view
     }
+
 }
